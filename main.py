6255593530415802e27da5c2f314bc5520674b77
sloudel <sloudel@yandex.ru>
2023-12-29 14:22:44 +0300

    fix words replacement implementation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import re
+
 import requests
 from flask import Flask, request
-from bs4 import BeautifulSoup
 
 base_url = 'https://news.ycombinator.com'
 app_host = '127.0.0.1'
@@ -16,27 +17,23 @@ def main(path=None):
     Proxy all requests to Hacker News portal
     and add ™ to words with length == 6
     """
-    print(request.path)
-    print(request.query_string)
     response_content = requests.get(
-        f'{base_url}{request.path}?{request.query_string.decode("utf-8")}'
-    ).content
-    try:
-        response_content = response_content.decode('utf-8')
-        response_content = response_content.replace(
-            base_url, f'http://{app_host}:{app_port}'
-        )
-    except UnicodeDecodeError:
-        return bytes(response_content)  # return images, styles etc.
-    soup = BeautifulSoup(response_content, 'html.parser')
-    comments = soup.find_all(attrs={'class': 'comment'})
-    replaced = set()
-    for comment in comments or []:
-        source_text = comment.get_text()
-        for word in source_text.split(' '):
-            if len(word) == 6 and word not in replaced:
-                replaced.add(word)
-                response_content = response_content.replace(word, word + '™')
+        f'{base_url}{request.path}'
+        f'?{request.query_string.decode("utf-8")}'
+    ).content.decode('utf-8')
+
+    # replace hacker news portal urls with proxy url
+    response_content = response_content.replace(
+        base_url, f'http://{app_host}:{app_port}'
+    )
+    # replace one word in a tag
+    response_content = re.sub(
+        r'>(\b(\w{6})\b)<', r'>\1™<', response_content, flags=re.I
+    )
+    # replace len(word) == 6 and space after it
+    response_content = re.sub(
+        r'(\b(\w{6})\b) ', r'\1™ ', response_content, flags=re.I
+    )
     return bytes(response_content, 'utf-8')
 
 
